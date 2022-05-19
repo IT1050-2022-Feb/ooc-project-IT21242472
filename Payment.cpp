0ebc96f8edff700f810feab671a46d55baf11c06
IT21242472 <99546116+IT21242472@users.noreply.github.com>
2022-05-19 17:05:38 +0000

    MLB_WD_CSNE_13_02

---
+++ Payment.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+//IT21242472
+#include<cstring>
+#include"payment.h"
+
+using namespace std;
+
+//.cpp
+Payment :: Payment()
+{
+	
+}
+Payment :: Payment()
+{
+	strcpy(payId, " ");
+	strcpy(paymentMethod, " ");
+	strcpy(paymentDate, " ");
+	advancedFee = 0.0;
+	totalFee = 0.0;
+}
+Payment :: Payment(char pId[], char pDate[], float ptotalFee)
+{
+	payId = pId;
+	paymentDate = pDate;
+	totalFee = ptotalFee;
+}
+void Payment :: generatePayId()
+{
+	
+}
+void Payment :: storePaymentDetails()
+{
+	
+}
+void Payment ::  validate()
+{
+	
+}
+Payment :: ~Payment()
+{
+	//Destructor
+  cout << "Destructor runs" << endl;
+}
